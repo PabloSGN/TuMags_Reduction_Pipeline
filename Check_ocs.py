79e8b4041cfc321b1763142b005dde33bcdfed1a
PabloSGN <pablosantamarinag@gmail.com>
2024-10-09 15:42:47 +0200

    added save option to separate ocs from terminal

--- Check_ocs.py
+++ Check_ocs.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import pickle
 import image_handler as ih
 
 if __name__ == "__main__":
@@ -12,3 +14,14 @@ if __name__ == "__main__":
    
     paths = ih.get_images_paths(args[1])
     ocs = ih.separate_ocs(paths, verbose = True, flat_fieldmode = flatflag)
+
+    if "save" in args:
+        count = 1
+
+        filename = "OCs.pickle"
+        while os.path.exists(filename):
+            filename = f"OCs_{count}.pickle"
+            count += 1
+        
+        with open(filename, "wb") as output_file:
+            pickle.dump(ocs, output_file)
