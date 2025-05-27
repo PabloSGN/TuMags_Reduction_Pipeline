c421bb08b36e3a25cc5633113ae1c316f717b5be
David Orozco <d.orozco.suarez@gmail.com>
2025-05-27 14:43:42 +0200

    added logging utils

--- src/tumagpipe/__init__.py
+++ src/tumagpipe/__init__.py
@@ -6,3 +6,17 @@ __version__ = "0.1.0"
 
 from . import (image_handler,master_dark,master_flatfield,image_filtering,destretch,demodulation)
 # from . import (alignment,Calibration_finder,Check_ocs,config,demodulation,destretch,image_filtering,image_handler,master_dark,master_flat)
+
+import logging
+from .logutils import log_memory
+
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.info("mypackage initialized.")
+
