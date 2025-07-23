d582310692a5539e00b2a92dc38d0128b31dbef2
David Orozco <d.orozco.suarez@gmail.com>
2025-07-23 14:49:29 +0200

    modified init for branch rearangement

--- src/tumagpipe/__init__.py
+++ src/tumagpipe/__init__.py
@@ -4,7 +4,7 @@ TuMagPipe package: tools for image destretching and reduction pipeline.
 
 __version__ = "0.1.0"
 
-from . import (image_handler,master_dark,master_flatfield,image_filtering,destretch,demodulation)
+from . import (image_handler,master_dark,master_flatfield,image_filtering,destretch,demodulation,fits_files_handling,prefilter_removal,config,pd_functions_v22,zernike,math_func2,shift_func)
 # from . import (alignment,Calibration_finder,Check_ocs,config,demodulation,destretch,image_filtering,image_handler,master_dark,master_flat)
 
 import logging
