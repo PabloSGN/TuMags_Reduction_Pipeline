5e663a65f4f3036adf6fcc5d80a4d083e76573cf
Francisco Javier Bailén Martínez <163529276+fjbailen@users.noreply.github.com>
2025-09-08 13:52:52 +0200

    Fixed stray light bugs
    -Fixed bug in stray_light function
    -Fixed default value of epsilon and sigma in restore_ima

--- pd_functions_v23.py
+++ pd_functions_v23.py
@@ -1738,7 +1738,7 @@ def OTF_stray(sigma,RHO,stray='moffat'):
     otf_stray=otf_stray[:,:,np.newaxis] #Dummy axis to have same dimensions as the OTF    
     return otf_stray,norma_stray
 
-def OTF_tot(epsilon,OTF_wave,OTF_stray,OTF_diff):
+def OTF_tot(epsilon,OTF_wave,OTF_stray):
     """
     Computes the total OTF of the system, taking into
     account both aberrations and stray light.
@@ -1747,7 +1747,7 @@ def OTF_tot(epsilon,OTF_wave,OTF_stray,OTF_diff):
         OTF_wave: OTF corresponding to aberrations (OTF)
         OTF_stray: OTF corresponding to stray light
     """
-    return (1-epsilon)*OTF_wave+epsilon*OTF_diff*OTF_stray  
+    return (1-epsilon)*OTF_wave+epsilon*OTF_wave*OTF_stray  
 
 def object_estimate(ima,a,a_d,wind=True,cobs=0,cut=29,low_f=0.2,tiptilt=False,
                     noise='default',reg1=0,reg2=1,inst='tumag',epsilon=0,sigma=0,
@@ -1774,7 +1774,7 @@ def object_estimate(ima,a,a_d,wind=True,cobs=0,cut=29,low_f=0.2,tiptilt=False,
         noise: 'default' to be computed as filt_scharmer. Otherwise, this variable
             should contain a 2x2 array with the filter.
         inst: instrument for which we compute the parameters
-        epsilon: fraction of stray light (0<epsilon<1)
+        epsilon: fraction of stray light (0<epsilon<1). 0 by default
         sigma: parameter defining the width of the stray light term (in pixels)
         stray: type of straylight ('gaussian' or 'moffat')
 
@@ -1812,9 +1812,8 @@ def object_estimate(ima,a,a_d,wind=True,cobs=0,cut=29,low_f=0.2,tiptilt=False,
     RHO2=RHO2[:-1,:-1]
 
     Hk_wave,_=OTF(a,a_d,RHO,THETA,ap,norm=True,K=Ok.shape[2],tiptilt=tiptilt)
-    Hk_diff,_=OTF(0*a,a_d,RHO,THETA,ap,norm=True,K=Ok.shape[2],tiptilt=tiptilt)
     Hk_stray,_=OTF_stray(sigma_arcsec,RHO2,stray=stray)
-    Hk=OTF_tot(epsilon,Hk_wave,Hk_stray,Hk_diff)
+    Hk=OTF_tot(epsilon,Hk_wave,Hk_stray)
 
     
     #Restoration
@@ -2141,7 +2140,7 @@ def padding(ima):
     return ima_pad,pw
 
 def restore_ima(ima,zernikes,pd=0,low_f=0.2,noise='default',reg1=0.05,
-                reg2=1,cobs=32.4):
+                reg2=1,cobs=32.4,epsilon=0,sigma=0,stray='moffat'):
     """
     This function restores a 2D or a 3D image using a given set of Zernike 
     coefficients and a modified Wiener filter that includes a 
@@ -2158,8 +2157,9 @@ def restore_ima(ima,zernikes,pd=0,low_f=0.2,noise='default',reg1=0.05,
         reg1: 1st parameter of regularization term
         reg2: 2nd parameter of the regularization term 
         cobs: percentage of the central obscuration
-        epsilon:
-        sigma:
+        epsilon: fraction of the stray light (0<epsilon<1). 0 by default
+        sigma: parameter defining the width of the stray light term (in pixels)
+        stray: type of straylight ('gaussian' or 'moffat')
     Output:
         ima_rest: restored image over the whole FOV of the input image
         noise_filt: noise filter employed for the restoration
@@ -2171,7 +2171,8 @@ def restore_ima(ima,zernikes,pd=0,low_f=0.2,noise='default',reg1=0.05,
     #If we select only one image of the series
     ima_rest,_,noise_filt=object_estimate(ima_pad,zernikes,pd,wind=True,
                                           cobs=cobs,cut=cut,low_f=low_f,
-                                          noise=noise,reg1=reg1,reg2=reg2,epsilon=0,sigma=0)
+                                          noise=noise,reg1=reg1,reg2=reg2,
+                                          epsilon=epsilon,sigma=sigma,stray=stray)
     ima_rest=ima_rest[cut:-cut,cut:-cut]
     return ima_rest, noise_filt
 
